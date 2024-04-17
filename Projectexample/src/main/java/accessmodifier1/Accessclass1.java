9f300d2b02dbc34ab8c4875068716bb67afa9964
arunima1996 <167090136+arunima1996@users.noreply.github.com>
2024-04-17 21:10:07 +0400

    Accessspecifier

---
+++ Projectexample/src/main/java/accessmodifier1/Accessclass1.java
@@ -0,0 +1,15 @@
+package accessmodifier1;
+
+public class Accessclass1 
+{
+
+	public static void main(String[] args) 
+	{
+		Accessclass obj=new Accessclass();
+		obj.display1();
+		obj.dispaly3();
+		obj.dispaly4();
+		//obj.dispaly2();
+	}
+
+}
---
+++ Projectexample/src/main/java/accessmodifier2/Accessclass2.java
@@ -0,0 +1,17 @@
+package accessmodifier2;
+
+import accessmodifier1.Accessclass;
+
+public class Accessclass2 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		Accessclass obj=new Accessclass();
+		obj.display1();
+		
+
+	}
+
+}
